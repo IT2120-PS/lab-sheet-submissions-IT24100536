ae4509d5051b6b0205a2e8597b413075520039d1
IT24100536 <IT24100536@my.sliit.lk>
2025-08-28 15:19:11 +0530

    Add files via upload

---
+++ Lab Sheet 05/it24100536 R.R
@@ -0,0 +1,83 @@
+setwd("C:\\Users\\IT24100536\\Desktop\\IT24100536")
+getwd()
+data<-read.table("Data.txt",header = TRUE, sep = ",")
+fix(data)
+attach(data)
+
+names(data) <-c("X1","X2")
+attach(data)
+hist(X2,main="Histogram for Number of Shareholders")
+
+histogram<-hist(X2,main="Histogram for number of Shareholders",breaks=seq(130,270,length=8),right=FALSE)
+
+breaks<-round(histogram$breaks)
+freq<-histogram$counts
+mids<-histogram$mids
+
+classes<-c()
+for(i in 1:length(breaks)-1){
+  classes[i]<-paste0("[",breaks[i],",",breaks[i+1],")")
+}
+lines(mids,freq)
+plot(mids,freq,type='l',main="Frequency polygon for Shareholders",xlab="Shareholders",ylab="Frequency",ylim=c(0,max(freq)))
+
+
+cum.freq<-cumsum(freq)
+new<-c()
+for(i in 1:length(breaks)){
+  if(i==1){
+    new[i]=0
+  }else{
+    new[i]=cum.freq[i-1]
+  } 
+}
+plot(breaks,new,type='l',main="Cumulative frequency Polygon for shareholders",)
+xlab="Shareholders",ylab="cumulative frequency",ylim=c(0,max(cum.freq)))
+cbind(Upper=breaks,CumFreq=new)
+
+#Exercise 05
+
+##1
+setwd("C:\\Users\\IT24100536\\Desktop\\IT24100536")
+Delivery_Times <- read.table("Exercise - Lab 05.txt", header = TRUE)
+fix(Delivery_Times)
+attach(Delivery_Times)
+##2
+names(Delivery_Times) <- "Delivery_Time_minutes"
+fix(Delivery_Times)
+attach(Delivery_Times)
+Delivery_hist <- hist(Delivery_Time_minutes,main = "Histogram of Delivery Times", 
+                      xlab = "Delivery Time (minutes)", ylab = "Frequency",
+                      breaks = seq(20, 70, length = 10),right = FALSE)
+plot(breaks_delivery, shifted_cum_freq, type = 'l', main = "Cumulative Frequency Polygon for Delivery Times", 
+     xlab = "Delivery Time (minutes)", ylab = "Cumulative Frequency", ylim = c(0, max(cum_freq_delivery))) 
+
+
+##4
+breaks <- round(histogram$breaks)
+freq <- histogram$counts
+mids <- histogram$mids
+cum_freq <- cumsum(freq)
+ogive_breaks <- breaks
+ogive_cum_freq <- c(0, cum_freq)
+
+plot(ogive_breaks, ogive_cum_freq,
+     type = "l",
+     main = "Cumulative Frequency Polygon (Ogive) of Delivery Times",
+     xlab = "Delivery Time (minutes)",
+     ylab = "Cumulative Frequency",
+     xlim = c(20, 70),
+     ylim = c(0, max(ogive_cum_freq)))
+grid()
+points(ogive_breaks, ogive_cum_freq, pch = 16, col = "red")
+text(ogive_breaks, ogive_cum_freq, labels = ogive_cum_freq, pos = 3, cex = 0.8)
+classes <- c()
+for(i in 1:(length(breaks)-1)) {
+  classes[i] <- paste0("[", breaks[i], ", ", breaks[i+1], ")")
+}
+freq_table <- data.frame(Classes = classes, Frequency = freq)
+print(freq_table)
+
+cat("\nCumulative Frequency Table:\n")
+cum_table <- data.frame(Upper_Limit = breaks, Cumulative_Frequency = ogive_cum_freq)
+print(cum_table)
